b86b7ab02e2481f417d249007ad5b18830caa983
Sam Ceron <samceronm@gmail.com>
2021-01-19 13:02:02 -0600

    Updating bubble and bar charts

--- script/app.js
+++ script/app.js
@@ -2,7 +2,7 @@
 
 function init() {
     //console.log("in")
-
+    
     // Set the dropdown menu
     let selector = d3.select("#selDataset");
     
@@ -10,15 +10,63 @@ function init() {
     d3.json("/data/data_samples.json").then((data) => {
         let subjectIds = data.names;
         subjectIds.forEach((id) => {
-          selector
-          .append("option")
-          .text(id)
-          .property("value", id);
+            selector
+            .append("option")
+            .text(id)
+            .property("value", id);
         });
-
+    let firstSample = subjectIds[0];
+    updateChart(firstSample);
     });
-  }
-  
-  
-  // Initialize the dashboard
-  init();
+}
+
+function updateChart(sample) {    
+    d3.json("/data/data_samples.json").then((data) => {
+        //Set data from the sample selected
+        let samples = data.samples;
+        let filterArray = samples.filter(sampleObject => sampleObject.id == sample);
+        let sample_selected = filterArray[0];
+        let sample_values = sample_selected.sample_values;
+        let otu_ids = sample_selected.otu_ids;
+        let otu_labels = sample_selected.otu_labels;   
+        //Build Bubble Chart
+        var trace1 = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: 'markers',
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale:"Rainbow"
+            }
+        };
+        var data = [trace1];
+        var layout = {
+            title: 'Bacteria Cultures per Sample',
+            showlegend: false,
+            hovermode: 'closest',
+            xaxis: {title:"OTU (Operational Taxonomic Unit) ID " +sample},
+            margin: {t:30}
+        };
+        Plotly.newPlot('bubble', data, layout); 
+        //Build Bar Chart
+        var trace1 = {
+            x: sample_values.slice(0,10).reverse(),
+            y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+            text: otu_labels.slice(0,10).reverse(),
+            type: "bar",
+            orientation: "h",
+            marker:{
+                color: 'rgba(255,153,51,0.6)',
+                width:1
+            }
+        };
+        var data = [trace1];
+        var layout = {
+        };
+        Plotly.newPlot("bar", data, layout);  
+    });
+}
+// Initialize the dashboard
+init();
